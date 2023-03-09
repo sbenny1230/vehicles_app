91ac7b09c1b02956151c5be4d2a37c6d8a4f6cd9
Susmi Benny <susmibenny1999@gmail.com>
2023-03-09 12:41:18 +0000

    index.php: Add: Request and send response

--- public/index.php
+++ public/index.php
@@ -1,11 +1,18 @@
 <?php declare(strict_types=1);
 
+use SB\Framework\Http\Request;
+use SB\Framework\Http\Response;
+
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
-// Request received
+
+// Get request
 $request = Request::createFromGlobals();
 
+$content = '<h1>Hello World</h1>';
 
-echo 'Hello world!!!';
+// Get response
+$response = new Response(content: $content, status: 200, headers: []);
+$response->send();
 
 ?>
