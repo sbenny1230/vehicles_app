fc4b92e4e23718f2d39327ebef722d3a8cd33208
Susmi Benny <susmibenny1999@gmail.com>
2023-03-09 12:41:00 +0000

    Response.php: Add: Initial class

---
+++ framework/http/Response.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace SB\Framework\Http;
+
+class Response
+{
+    public function __construct(
+        private ?string $content = '',
+        private int $status = 200,
+        private array $headers = []
+    )
+    {
+    }
+
+    public function send(): void 
+    {
+        echo $this->content;
+    }
+}
+
+?>
+
